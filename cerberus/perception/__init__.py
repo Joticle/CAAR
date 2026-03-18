4c4bf179be3b8d6545c1f71bbdfea5bc754cb211
scott <scott@joticle.com>
2026-03-18 16:00:30 -0700

    INi mod 2

--- cerberus/perception/__init__.py
+++ cerberus/perception/__init__.py
@@ -4,11 +4,11 @@ Pi Camera 3, GPS, BME680/SCD-40/SHT45 environmental sensors.
 """
 
 from cerberus.perception.camera import CerberusCamera
-from cerberus.perception.gps import GPS
+from cerberus.perception.gps import CerberusGPS
 from cerberus.perception.environment import EnvironmentManager
 
 __all__ = [
     "CerberusCamera",
-    "GPS",
+    "CerberusGPS",
     "EnvironmentManager",
 ]
